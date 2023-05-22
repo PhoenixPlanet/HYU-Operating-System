2543be10591a884aa789a70efb31c274e45a624f
2019039843 <ytw0113@hanyang.ac.kr>
2023-05-22 09:14:21 -0700

    test: Add exec test files

--- Project2/xv6-public/pmanager.c
+++ Project2/xv6-public/pmanager.c
@@ -4,27 +4,32 @@
 
 #include "pmanager.h"
 
-static char* pmanager_cmds[] = {
-[PM_HELP]   "help",
-[PM_LIST]   "list",
-[PM_KILL]   "kill",
-[PM_EXEC]   "exec",
-[PM_MEMLIM] "memlim",
-[PM_EXIT]   "exit",
-};
+// static char* pmanager_cmds[] = {
+// [PM_HELP]   "help",
+// [PM_LIST]   "list",
+// [PM_KILL]   "kill",
+// [PM_EXEC]   "exec",
+// [PM_MEMLIM] "memlim",
+// [PM_EXIT]   "exit",
+// };
 
-static char* pmanager_help_msgs[] = {
-[PM_HELP]   "print help messages for pmanager commands",
-[PM_LIST]   "list information of all processes",
-[PM_KILL]   "kill target pid process",
-[PM_EXEC]   "execute target program with given stack size",
-[PM_MEMLIM] "memlim",
-[PM_EXIT]   "exit",
-};
+// static char* pmanager_help_msgs[] = {
+// [PM_HELP]   "print help messages for pmanager commands",
+// [PM_LIST]   "list information of all processes",
+// [PM_KILL]   "kill target pid process",
+// [PM_EXEC]   "execute target program with given stack size",
+// [PM_MEMLIM] "memlim",
+// [PM_EXIT]   "exit",
+// };
 
 int
 main(int argc, char *argv[]) {
+  char c;
   printf(1, "\nProcess Manager\n");
+  
+  while(read(0, &c, 1)) {
+    printf(1, "input: %c\n", c);
+  }
 
   exit();
 }
