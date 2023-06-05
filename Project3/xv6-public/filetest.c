0660a6543fcec55e36b8a8a071cecd14e061d319
2019039843 <ytw0113@hanyang.ac.kr>
2023-06-05 11:05:24 -0700

    test: Modify test file to test sync and symbolic link correctly

--- Project3/xv6-public/filetest.c
+++ Project3/xv6-public/filetest.c
@@ -4,72 +4,11 @@
 #include "fcntl.h"
 #include "fs.h"
 
-void
-big_file_test(void)
-{
-  int i, fd, n;
-  char buf[BSIZE];
-
-  printf(2, "big files test\n");
-
-  fd = open("big", O_CREATE|O_RDWR);
-  if(fd < 0){
-    printf(2, "error: creat big failed!\n");
-    exit();
-  }
-
-  for(i = 0; i < 20000; i++){
-    ((int*)buf)[0] = i;
-    if(write(fd, buf, 512) != 512){
-      printf(2, "error: write big file failed\n", i);
-      exit();
-    }
-    if (i % 5000 == 0)
-      printf(2, "write: %d / %d\n", i, 20000);
-  }
-
-  close(fd);
-
-  fd = open("big", O_RDONLY);
-  if(fd < 0){
-    printf(2, "error: open big failed!\n");
-    exit();
-  }
-
-  n = 0;
-  for(;;){
-    i = read(fd, buf, 512);
-    if(i == 0){
-      if(n == 20000 - 1){
-        printf(2, "read only %d blocks from big", n);
-        exit();
-      }
-      break;
-    } else if(i != 512){
-      printf(2, "read failed %d\n", i);
-      exit();
-    }
-    if(((int*)buf)[0] != n){
-      printf(2, "read content of block %d is %d\n",
-             n, ((int*)buf)[0]);
-      exit();
-    }
-    n++;
-    if (n % 5000 == 0)
-      printf(2, "read: %d / %d\n", n, 20000);
-  }
-  close(fd);
-  if(unlink("big") < 0){
-    printf(2, "unlink big failed\n");
-    exit();
-  }
-  printf(2, "big files ok\n");
-}
-
+#define BIGBLOCK 20000
 #define SMALLBLOCK 30
 
 void
-write_small_test(char* name)
+write_test(char* name, int file_block_size, int do_sync)
 {
   int i, fd;
   int buf[BSIZE];
@@ -82,26 +21,29 @@ write_small_test(char* name)
     exit();
   }
 
-  for(i = 0; i < SMALLBLOCK; i++){
+  for(i = 0; i < file_block_size; i++){
     buf[(BSIZE/sizeof(int)) - 1] = i;
     if(write(fd, (char*)buf, 512) != 512){
       printf(2, "error: write small file failed\n", i);
       exit();
     }
-    if (i % 1000 == 0)
-      printf(2, "write: %d / %d\n", i, SMALLBLOCK);
+    if (i % (file_block_size / 10) == 0 || (i == file_block_size - 1))
+      printf(2, "write: %d / %d\n", (i == file_block_size - 1) ? file_block_size : i, file_block_size);
   }
-  //printf(2, "flushed %d\n", sync());
+  if (do_sync) {
+    printf(2, "flushed %d\n", sync());
+  }
+  
   close(fd);
 }
 
-void read_small_test(char* name) {
+void read_test(char* name, int file_block_size) {
   int i, fd, n;
   int buf[BSIZE/sizeof(int)];
 
   fd = open(name, O_RDONLY);
   if(fd < 0){
-    printf(2, "error: open small failed!\n");
+    printf(2, "error: open %s failed!\n", name);
     exit();
   }
 
@@ -109,8 +51,8 @@ void read_small_test(char* name) {
   for(;;){
     i = read(fd, (char*)buf, 512);
     if(i == 0){
-      if(n == SMALLBLOCK - 1){
-        printf(2, "read only %d blocks from small", n);
+      if(n == file_block_size - 1){
+        printf(2, "read only %d blocks from %s\n", n, name);
         exit();
       }
       break;
@@ -124,15 +66,15 @@ void read_small_test(char* name) {
       exit();
     }
     n++;
-    if (n % 1000 == 0)
-      printf(2, "read: %d / %d\n", n, SMALLBLOCK);
+    if (n % (file_block_size / 10) == 0)
+      printf(2, "read: %d / %d\n", n, file_block_size);
   }
   close(fd);
   if(unlink(name) < 0){
-    printf(2, "unlink small failed\n");
+    printf(2, "unlink %s failed\n", name);
     exit();
   }
-  printf(2, "small files ok\n");
+  printf(2, "%s files ok\n", name);
 }
 
 void get_filename_test(char* target) {
@@ -142,9 +84,13 @@ void get_filename_test(char* target) {
   printf(1, "%s\n", real);
 }
 
+char* test_dir = "test";
+char* test_dir2 = "test/test2";
 char* test_file = "symtest.txt";
-char* test_link_file = "test.sym";
-char* test_str = "hello symbolic link";
+char* test_target = "../test2/./symtest.txt";
+char* test_link_file = "/test.sym";
+char* test_link_open_target = "test.sym";
+char* test_str = "hello symbolic link\n";
 
 void symlink_test() {
   char buf[25];
@@ -152,39 +98,46 @@ void symlink_test() {
 
   memset(buf, 0, sizeof(buf));
 
+  mkdir(test_dir);
+  mkdir(test_dir2);
+  chdir(test_dir2);
+
   fd = open(test_file, O_CREATE | O_WRONLY);
   if(fd < 0){
-    printf(1, "failed to open %s (create | write)\n", test_file);
+    printf(2, "failed to open %s (create | write)\n", test_file);
     exit();
   }
 
   write(fd, test_str, strlen(test_str) + 1);
 
-  printf(1, "write <%s> finish\n", test_str);
+  printf(2, "write <%s> finish\n", test_str);
+  printf(2, "flushed %d\n", sync());
   
   close(fd);
 
-  symbolic_link(test_file, test_link_file);
+  symbolic_link(test_target, test_link_file);
 
-  fd = open(test_link_file, O_RDONLY);
+  chdir("/");
+
+  fd = open(test_link_open_target, O_RDONLY);
   if(fd < 0){
-    printf(1, "failed to open %s (read)\n", test_file);
+    printf(2, "failed to open %s (read)\n", test_file);
     exit();
   }
 
   if (read(fd, buf, strlen(test_str) + 1) <= 0) {
-    printf(1, "failed to read\n");
+    printf(2, "failed to read\n");
     exit();
   }
 
   if (strcmp(buf, test_str) != 0) {
-    printf(1, "wrong contents: %s\n", buf);
+    printf(2, "wrong contents: %s\n", buf);
     exit();
   }
 
   close(fd);
 
-  printf(1, "symbolic link test passed\n"); 
+  printf(2, "symbolic link test passed\n"); 
 }
 
 int
@@ -201,11 +154,23 @@ main(int argc, char *argv[])
   //big_file_test();
 
   if (strcmp(argv[1], "w") == 0) {
-    write_small_test("small1");
+    if (strcmp(argv[2], "t") == 0) {
+      write_test("big", BIGBLOCK, 1);
+    } else if (strcmp(argv[2], "f") == 0) {
+      write_test("big", BIGBLOCK, 0);
+    }
   }
 
   if (strcmp(argv[1], "r") == 0) {
-    read_small_test("small1");
+    read_test("big", BIGBLOCK);
+  }
+
+  if (strcmp(argv[1], "s") == 0) {
+    symlink_test();
+  }
+
+  if (strcmp(argv[1], "n") == 0) {
+    get_filename_test(argv[2]);
   }
 
   exit();
